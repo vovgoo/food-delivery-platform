047b1576fbe5cba8e9e84eed669dabd49e1be0bd
Vova <vov.gonchar2017@yandex.ru>
2025-11-11 15:59:07 +0300

    refactor UserRepository, fix problem with N+1 problem in CustomUserDetailsService

--- backend/user-service/src/main/java/org/vovgoo/userservice/service/security/impl/CustomUserDetailsService.java
+++ backend/user-service/src/main/java/org/vovgoo/userservice/service/security/impl/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
+        return userRepository.findByEmailWithRoles(email)
                 .orElseThrow(() -> new UsernameNotFoundException(email));
     }
 }
