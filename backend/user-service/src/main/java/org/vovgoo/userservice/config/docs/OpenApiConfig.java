cac631bf8f4ecc4339709f10271001fbf5f5d9ed
Vova <vov.gonchar2017@yandex.ru>
2025-11-11 15:58:04 +0300

    Configurate base OpenApiConfig

---
+++ backend/user-service/src/main/java/org/vovgoo/userservice/config/docs/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package org.vovgoo.userservice.config.docs;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .version("v1.0")
+                        .description("API для работы с пользователями, регистрацией, профилем и адресами.")
+                        .contact(new Contact()
+                                .name("vovgoo")
+                                .email("vov.gonchar05@gmail.com")))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+    }
+}
