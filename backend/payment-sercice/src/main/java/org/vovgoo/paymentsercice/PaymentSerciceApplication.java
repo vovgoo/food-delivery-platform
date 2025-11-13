7c7f672ad13840ce9b38c1842dfed7719a28c4da
Vova <vov.gonchar2017@yandex.ru>
2025-11-13 08:26:50 +0300

    add payment service

---
+++ backend/payment-sercice/src/main/java/org/vovgoo/paymentsercice/PaymentSerciceApplication.java
@@ -0,0 +1,13 @@
+package org.vovgoo.paymentsercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSerciceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSerciceApplication.class, args);
+    }
+
+}
---
+++ backend/payment-sercice/src/test/java/org/vovgoo/paymentsercice/PaymentSerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vovgoo.paymentsercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSerciceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
