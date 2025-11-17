ab9085e9b613bbfaf28d8aa36cb60ca9aff29c84
Vova <vov.gonchar2017@yandex.ru>
2025-11-17 21:16:18 +0300

    refactor mapper for new responses

--- backend/user-service/src/main/java/org/vovgoo/userservice/mapper/UserMapper.java
+++ backend/user-service/src/main/java/org/vovgoo/userservice/mapper/UserMapper.java
@@ -3,17 +3,21 @@ package org.vovgoo.userservice.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.vovgoo.userservice.dto.user.response.UserResponse;
+import org.vovgoo.userservice.entity.Address;
 import org.vovgoo.userservice.entity.User;
 
 @Mapper(componentModel = "spring", uses = {AddressMapper.class, RoleMapper.class})
 public interface UserMapper {
 
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "email", source = "email")
-    @Mapping(target = "fullName", source = "fullName")
-    @Mapping(target = "createdAt", source = "createdAt")
-    @Mapping(target = "updatedAt", source = "updatedAt")
-    @Mapping(target = "addresses", source = "addresses")
-    @Mapping(target = "roles", source = "roles")
-    UserResponse toResponse(User user);
+    @Mapping(target = "defaultAddress", source = "defaultAddress")
+    @Mapping(target = "id", source = "user.id")
+    @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "phone", source = "user.phone")
+    @Mapping(target = "fullName", source = "user.fullName")
+    @Mapping(target = "birthDate", source = "user.birthDate")
+    @Mapping(target = "userStatus", source = "user.status")
+    @Mapping(target = "createdAt", source = "user.createdAt")
+    @Mapping(target = "updatedAt", source = "user.updatedAt")
+    @Mapping(target = "roles", source = "user.roles")
+    UserResponse toResponse(User user, Address defaultAddress);
 }
