23905e311b52d5acbf7b5b583aed1bde111d1d18
Vova <vov.gonchar2017@yandex.ru>
2025-11-17 15:44:25 +0300

    realize auth service

---
+++ backend/user-service/src/main/java/org/vovgoo/userservice/service/security/auth/AuthService.java
@@ -0,0 +1,14 @@
+package org.vovgoo.userservice.service.security.auth;
+
+import org.vovgoo.userservice.dto.security.auth.request.*;
+import org.vovgoo.userservice.dto.security.jwt.internal.JwtPair;
+import org.vovgoo.userservice.dto.security.auth.response.PhoneVerificationResponse;
+import org.vovgoo.userservice.dto.security.jwt.response.JwtResponse;
+
+public interface AuthService {
+    JwtPair signIn(SignInRequest signInRequest);
+    PhoneVerificationResponse signUp(SignUpRequest signUpRequest);
+    void resendSignUpOtpCode(String token);
+    JwtPair confirmSignUp(String token, ConfirmSignUpRequest confirmSignUpRequest);
+    JwtResponse refreshAccessToken(String refreshToken);
+}
