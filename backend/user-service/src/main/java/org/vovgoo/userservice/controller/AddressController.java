567fe0571f5aaca3fc8c1b6c313ff3f7a4e15f95
Vova <vov.gonchar2017@yandex.ru>
2025-11-17 23:16:17 +0300

    create AddressController

---
+++ backend/user-service/src/main/java/org/vovgoo/userservice/controller/AddressController.java
@@ -0,0 +1,52 @@
+package org.vovgoo.userservice.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.vovgoo.userservice.dto.address.request.CreateAddressRequest;
+import org.vovgoo.userservice.dto.address.response.AddressResponse;
+import org.vovgoo.userservice.dto.common.PageParams;
+import org.vovgoo.userservice.dto.common.PageResponse;
+import org.vovgoo.userservice.service.address.AddressService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/address")
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @GetMapping("/me/get")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<PageResponse<AddressResponse>> getAll(@Valid PageParams pageParams) {
+        PageResponse<AddressResponse> response = addressService.getAll(pageParams);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/me/create")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<AddressResponse> createAddress(@Valid @RequestBody CreateAddressRequest createAddressRequest) {
+        AddressResponse response = addressService.create(createAddressRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/me/remove/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> remove(@PathVariable UUID id) {
+        addressService.remove(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/me/default/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> setDefault(@PathVariable UUID id) {
+        addressService.setDefault(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
