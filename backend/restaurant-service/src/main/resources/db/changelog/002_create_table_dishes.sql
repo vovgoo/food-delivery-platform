b28396cc2ba3cb9e4247336bdf365d782b148db4
Vova <vov.gonchar2017@yandex.ru>
2025-11-18 21:53:10 +0300

    update migrations for init database

--- backend/restaurant-service/src/main/resources/db/changelog/002_create_table_dishes.sql
+++ backend/restaurant-service/src/main/resources/db/changelog/002_create_table_dishes.sql
@@ -1,8 +1,17 @@
 CREATE TABLE dishes (
-    id BIGSERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(500),
+    profile_image_id UUID,
+    portion_in_grams INTEGER,
+    proteins DOUBLE PRECISION,
+    fats DOUBLE PRECISION,
+    carbohydrates DOUBLE PRECISION,
+    spicy BOOLEAN NOT NULL DEFAULT FALSE,
+    vegan BOOLEAN NOT NULL DEFAULT FALSE,
+    vegetarian BOOLEAN NOT NULL DEFAULT FALSE,
     price NUMERIC(8,2) NOT NULL,
-    image_url VARCHAR(500),
-    restaurant_id BIGINT NOT NULL
+    status VARCHAR(30) NOT NULL DEFAULT 'AVAILABLE',
+    restaurant_id UUID NOT NULL,
+    CONSTRAINT chk_dish_status CHECK (status IN ('AVAILABLE', 'TEMPORARY_UNAVAILABLE', 'REMOVED'))
 );
--- backend/restaurant-service/src/main/resources/db/changelog/003_add_constraints_restaurants.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE restaurants
-    ADD CONSTRAINT chk_restaurant_name_length CHECK (char_length(name) BETWEEN 2 AND 100),
-    ADD CONSTRAINT chk_restaurant_cuisine_length CHECK (char_length(cuisine) BETWEEN 2 AND 50),
-    ADD CONSTRAINT chk_restaurant_address_length CHECK (char_length(address) BETWEEN 5 AND 200);
