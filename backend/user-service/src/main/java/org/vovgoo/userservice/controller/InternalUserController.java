4d68c914663d98fa1543146506415a79be73cecc
Vova <vov.gonchar2017@yandex.ru>
2025-11-19 16:44:42 +0300

    realize internal users controller, which get user status

---
+++ backend/user-service/src/main/java/org/vovgoo/userservice/controller/InternalUserController.java
@@ -0,0 +1,27 @@
+package org.vovgoo.userservice.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.vovgoo.user.enums.UserStatus;
+import org.vovgoo.userservice.service.user.UserService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/internal/users")
+@RequiredArgsConstructor
+@Hidden
+public class InternalUserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}/status")
+    public ResponseEntity<UserStatus> getUserStatus(@PathVariable UUID userId) {
+        return ResponseEntity.ok(userService.getUserStatus(userId));
+    }
+}
