bcbddcd4b211d9859a7a89873a3e386ef59d6e25
Vova <vov.gonchar2017@yandex.ru>
2025-11-19 16:44:11 +0300

    change scan property in Application user service file

--- backend/user-service/src/main/java/org/vovgoo/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/org/vovgoo/userservice/UserServiceApplication.java
@@ -3,7 +3,7 @@ package org.vovgoo.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.vovgoo.userservice", "org.vovgoo.security"})
+@SpringBootApplication(scanBasePackages = {"org.vovgoo.userservice", "org.vovgoo.config", "org.vovgoo.security", "org.vovgoo.user"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
