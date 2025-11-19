fd27adafa4ff7ad49d05ae37a603fa30239395e2
Vova <vov.gonchar2017@yandex.ru>
2025-11-19 14:10:43 +0300

    create requests for create and update dish and restaurants

--- backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/dto/dish/request/DishUpdateRequest.java
+++ backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/dto/dish/request/DishUpdateRequest.java
@@ -1,7 +1,7 @@
 package org.vovgoo.restaurantservice.dto.dish.request;
 
 import jakarta.validation.constraints.*;
-import org.hibernate.validator.constraints.URL;
+import org.vovgoo.restaurantservice.entity.enums.DishStatus;
 
 import java.math.BigDecimal;
 
@@ -14,14 +14,32 @@ public record DishUpdateRequest(
         @Size(max = 500, message = "Описание блюда должно быть до 500 символов")
         String description,
 
+        @PositiveOrZero(message = "Вес блюда должен быть положительным или нулевым")
+        Integer portionInGrams,
+
+        @PositiveOrZero(message = "Количество белков должно быть положительным или нулевым")
+        Double proteins,
+
+        @PositiveOrZero(message = "Количество жиров должно быть положительным или нулевым")
+        Double fats,
+
+        @PositiveOrZero(message = "Количество углеводов должно быть положительным или нулевым")
+        Double carbohydrates,
+
+        @NotNull(message = "Необходимо указать, острое ли блюдо")
+        Boolean spicy,
+
+        @NotNull(message = "Необходимо указать, подходит ли блюдо для веганов")
+        Boolean vegan,
+
+        @NotNull(message = "Необходимо указать, подходит ли блюдо для вегетарианцев")
+        Boolean vegetarian,
+
         @NotNull(message = "Цена блюда обязательна")
         @DecimalMin(value = "0.01", message = "Цена блюда должна быть больше 0")
         @Digits(integer = 6, fraction = 2, message = "Цена должна быть числом с максимум 2 знаками после запятой")
         BigDecimal price,
 
-        @URL(message = "URL изображения должен быть валидным")
-        String imageUrl,
-
-        @NotNull(message = "Id ресторана обязателен")
-        Long restaurantId
+        @NotNull(message = "Статус ресторана не может быть пустым")
+        DishStatus status
 ) {}
--- backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/dto/restaurant/request/RestaurantCreateRequest.java
+++ backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/dto/restaurant/request/RestaurantCreateRequest.java
@@ -1,7 +1,12 @@
 package org.vovgoo.restaurantservice.dto.restaurant.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
+import org.vovgoo.validators.phone.Phone;
+
+import java.time.LocalTime;
 
 public record RestaurantCreateRequest(
 
@@ -9,11 +14,34 @@ public record RestaurantCreateRequest(
         @Size(min = 2, max = 100, message = "Название ресторана должно быть от 2 до 100 символов")
         String name,
 
+        @Size(max = 1000, message = "Описание ресторана не должно превышать 1000 символов")
+        String description,
+
         @NotBlank(message = "Кухня не может быть пустой")
         @Size(min = 2, max = 50, message = "Кухня должна быть от 2 до 50 символов")
         String cuisine,
 
         @NotBlank(message = "Адрес не может быть пустым")
         @Size(min = 5, max = 200, message = "Адрес должен быть от 5 до 200 символов")
-        String address
+        String address,
+
+        @Size(max = 200, message = "URL сайта не должен превышать 200 символов")
+        @URL(message = "Некорректный формат сайта")
+        String website,
+
+        @Phone
+        @NotBlank(message = "Телефон не может быть пустым")
+        String phone,
+
+        @NotNull(message = "Время открытия должно быть указано")
+        LocalTime openingTime,
+
+        @NotNull(message = "Время закрытия должно быть указано")
+        LocalTime closingTime,
+
+        @NotNull(message = "Необходимо указать, доступна ли доставка")
+        Boolean deliveryAvailable,
+
+        @NotNull(message = "Необходимо указать, доступна ли парковка")
+        Boolean parkingAvailable
 ) {}
