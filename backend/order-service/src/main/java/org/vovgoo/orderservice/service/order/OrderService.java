1da63f6293fde1c27cddbc51c8cab76d409d716c
Vova <vov.gonchar2017@yandex.ru>
2025-11-21 21:55:54 +0300

    refactor and realize order service

--- backend/order-service/src/main/java/org/vovgoo/orderservice/service/order/OrderService.java
+++ backend/order-service/src/main/java/org/vovgoo/orderservice/service/order/OrderService.java
@@ -6,9 +6,11 @@ import org.vovgoo.orderservice.dto.order.request.CreateOrderRequest;
 import org.vovgoo.orderservice.dto.order.request.UpdateOrderStatusRequest;
 import org.vovgoo.orderservice.dto.order.response.OrderResponse;
 
+import java.util.UUID;
+
 public interface OrderService {
     OrderResponse placeOrder(CreateOrderRequest createOrderRequest);
     PageResponse<OrderResponse> getAllOrders(PageParams pageParams);
-    OrderResponse getOrderById(Long orderId);
-    OrderResponse updateOrderStatus(Long orderId, UpdateOrderStatusRequest updateOrderStatusRequest);
+    OrderResponse getOrderById(UUID orderId);
+    OrderResponse updateOrderStatus(UUID orderId, UpdateOrderStatusRequest updateOrderStatusRequest);
 }
--- backend/order-service/src/main/java/org/vovgoo/orderservice/service/order/impl/OrderServiceImpl.java
+++ backend/order-service/src/main/java/org/vovgoo/orderservice/service/order/impl/OrderServiceImpl.java
@@ -1,36 +1,26 @@
 package org.vovgoo.orderservice.service.order.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.vovgoo.dto.address.AddressShortResponse;
-import org.vovgoo.dto.dish.DishShortResponse;
 import org.vovgoo.dto.pageable.PageParams;
 import org.vovgoo.dto.pageable.PageResponse;
-import org.vovgoo.dto.restaurant.RestaurantShortResponse;
 import org.vovgoo.orderservice.dto.order.request.CreateOrderRequest;
 import org.vovgoo.orderservice.dto.order.request.UpdateOrderStatusRequest;
 import org.vovgoo.orderservice.dto.order.response.OrderResponse;
-import org.vovgoo.orderservice.dto.orderItem.request.AddOrderItemRequest;
-import org.vovgoo.orderservice.dto.orderItem.response.OrderItemResponse;
 import org.vovgoo.orderservice.entity.Order;
-import org.vovgoo.orderservice.entity.OrderItem;
 import org.vovgoo.orderservice.entity.Payment;
-import org.vovgoo.orderservice.entity.enums.OrderStatus;
-import org.vovgoo.orderservice.exception.custom.dish.DishNotAvailableException;
-import org.vovgoo.orderservice.mapper.OrderItemMapper;
-import org.vovgoo.orderservice.mapper.OrderMapper;
+import org.vovgoo.orderservice.exception.custom.order.OrderNotFoundException;
 import org.vovgoo.orderservice.repository.OrderRepository;
-import org.vovgoo.orderservice.service.address.AddressService;
 import org.vovgoo.orderservice.service.order.OrderService;
+import org.vovgoo.orderservice.service.order.facade.OrderFacade;
 import org.vovgoo.orderservice.service.payment.PaymentService;
-import org.vovgoo.orderservice.service.restaurant.RestaurantService;
 import org.vovgoo.security.utils.CurrentUserUtils;
 import org.vovgoo.user.aspect.CheckUserStatus;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -39,11 +29,8 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
-    private final AddressService addressService;
-    private final RestaurantService restaurantService;
+    private final OrderFacade orderFacade;
     private final PaymentService paymentService;
-    private final OrderMapper orderMapper;
-    private final OrderItemMapper orderItemMapper;
 
     @Override
     @CheckUserStatus
@@ -51,100 +38,61 @@ public class OrderServiceImpl implements OrderService {
     public OrderResponse placeOrder(CreateOrderRequest createOrderRequest) {
         UUID userId = CurrentUserUtils.getCurrentUserId();
 
-        AddressShortResponse address = addressService.getValidAddress(userId, createOrderRequest.deliveryAddress());
-        RestaurantShortResponse restaurant = restaurantService.getRestaurantValid(createOrderRequest.restaurantId());
-        List<DishShortResponse> dishes = restaurantService.getAvailableDishesByRestaurant(
-                createOrderRequest.restaurantId(),
-                createOrderRequest.items().stream().map(AddOrderItemRequest::dishId).toList()
-        );
-
-        Order order = Order.builder()
-                .status(OrderStatus.CREATED)
-                .userId(userId)
-                .restaurantId(createOrderRequest.restaurantId())
-                .deliveryAddress(createOrderRequest.deliveryAddress())
-                .totalPrice(BigDecimal.ZERO)
-                .build();
-
-        List<OrderItem> orderItems = new ArrayList<>();
-        BigDecimal totalPrice = BigDecimal.ZERO;
-
-        for (AddOrderItemRequest itemRequest : createOrderRequest.items()) {
-            DishShortResponse dish = dishes.stream()
-                    .filter(d -> d.id().equals(itemRequest.dishId()))
-                    .findFirst()
-                    .orElseThrow(DishNotAvailableException::new);
-
-            OrderItem item = OrderItem.builder()
-                    .dishId(dish.id())
-                    .quantity(itemRequest.quantity())
-                    .price(dish.price())
-                    .order(order)
-                    .build();
-
-            orderItems.add(item);
-            totalPrice = totalPrice.add(item.getPrice().multiply(BigDecimal.valueOf(item.getQuantity())));
-        }
-
-        order.setItems(orderItems);
-        order.setTotalPrice(totalPrice);
+        Order order = orderFacade.buildOrder(userId, createOrderRequest);
 
         Payment payment = paymentService.processPayment(order, createOrderRequest.payment());
-
         order.setPayment(payment);
 
         order = orderRepository.save(order);
 
-        List<OrderItemResponse> orderItemResponses = orderItemMapper.toResponseList(order.getItems(), dishes);
-
-        return orderMapper.toResponse(order, address,  restaurant, orderItemResponses);
+        return orderFacade.assembleOrderResponse(order);
     }
 
     @Override
     @CheckUserStatus
     public PageResponse<OrderResponse> getAllOrders(PageParams pageParams) {
-//        Long userId = SecurityUtils.getCurrentUserId();
-//        boolean isAdmin = SecurityUtils.isAdmin();
-//
-//        PageRequest pageRequest = PageRequest.of(pageParams.page(), pageParams.size());
-//
-//        Page<Order> orderPage = isAdmin ? orderRepository.findAll(pageRequest) : orderRepository.findByUserId(userId, pageRequest);
-//
-//        Page<OrderResponse> content = orderPage.map(orderMapper::toResponse);
-//
-//        return PageResponse.of(content);
-        return null;
+        UUID userId = CurrentUserUtils.getCurrentUserId();
+
+        PageRequest pageRequest = PageRequest.of(pageParams.page(), pageParams.size());
+
+        Page<Order> orders;
+
+        if(CurrentUserUtils.isAdmin()) {
+            orders = orderRepository.findAllWithItemsAndPayment(pageRequest);
+        } else {
+            orders = orderRepository.findByUserIdWithItemsAndPayment(userId, pageRequest);
+        }
+
+        Page<OrderResponse> orderResponses = orders.map(orderFacade::assembleOrderResponse);
+
+        return PageResponse.of(orderResponses);
     }
 
     @Override
     @CheckUserStatus
-    public OrderResponse getOrderById(Long orderId) {
-//        Long userId = SecurityUtils.getCurrentUserId();
-//        boolean isAdmin = SecurityUtils.isAdmin();
-//
-//        Order order = orderRepository.findById(orderId)
-//                .orElseThrow(() -> new EntityNotFoundException("Заказ не найден"));
-//
-//        if (!isAdmin && !order.getUserId().equals(userId)) {
-//            throw new AccessDeniedException("Доступ запрещён");
-//        }
-//
-//        return orderMapper.toResponse(order);
+    public OrderResponse getOrderById(UUID orderId) {
+        UUID userId = CurrentUserUtils.getCurrentUserId();
 
-        return null;
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(OrderNotFoundException::new);
+
+        if(!order.getUserId().equals(userId) && !CurrentUserUtils.isAdmin()) {
+            throw new AccessDeniedException("Доступ запрещен.");
+        }
+
+        return orderFacade.assembleOrderResponse(order);
     }
 
     @Override
     @CheckUserStatus
     @Transactional
-    public OrderResponse updateOrderStatus(Long orderId, UpdateOrderStatusRequest updateOrderStatusRequest) {
-//        Order order = orderRepository.findById(orderId)
-//                .orElseThrow(() -> new EntityNotFoundException("Заказ не найден"));
-//
-//        order.setStatus(updateOrderStatusRequest.status());
-//        orderRepository.save(order);
-//
-//        return orderMapper.toResponse(order);
-        return null;
+    public OrderResponse updateOrderStatus(UUID orderId, UpdateOrderStatusRequest updateOrderStatusRequest) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(OrderNotFoundException::new);
+
+        order.setStatus(updateOrderStatusRequest.status());
+        order = orderRepository.save(order);
+
+        return orderFacade.assembleOrderResponse(order);
     }
 }
