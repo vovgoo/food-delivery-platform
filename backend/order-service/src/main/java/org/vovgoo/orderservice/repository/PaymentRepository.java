9443a237c3b708113291d568431c8d058fb5b745
Vova <vov.gonchar2017@yandex.ru>
2025-11-21 21:55:29 +0300

    remove payemnt repository

--- backend/order-service/src/main/java/org/vovgoo/orderservice/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.vovgoo.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.vovgoo.orderservice.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
-}
