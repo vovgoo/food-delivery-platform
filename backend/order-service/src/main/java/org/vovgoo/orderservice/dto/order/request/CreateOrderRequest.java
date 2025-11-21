dad5b3fa8a336ecd1ffb67a7a56b15743f55af14
Vova <vov.gonchar2017@yandex.ru>
2025-11-21 19:09:38 +0300

    refactor request dtos

--- backend/order-service/src/main/java/org/vovgoo/orderservice/dto/order/request/CreateOrderRequest.java
+++ backend/order-service/src/main/java/org/vovgoo/orderservice/dto/order/request/CreateOrderRequest.java
@@ -7,10 +7,14 @@ import org.vovgoo.orderservice.dto.orderItem.request.AddOrderItemRequest;
 import org.vovgoo.orderservice.dto.payment.request.AddPaymentRequest;
 
 import java.util.List;
+import java.util.UUID;
 
 public record CreateOrderRequest(
-        @NotNull(message = "ID ресторана не может быть пустым")
-        Long restaurantId,
+        @NotNull(message = "Идентификатор ресторана не может быть пустым")
+        UUID restaurantId,
+
+        @NotNull(message = "Идентификатор адреса не может быть пустым")
+        UUID deliveryAddress,
 
         @NotEmpty(message = "Список блюд не может быть пустым")
         @Valid
--- backend/order-service/src/main/java/org/vovgoo/orderservice/dto/orderItem/request/AddOrderItemRequest.java
+++ backend/order-service/src/main/java/org/vovgoo/orderservice/dto/orderItem/request/AddOrderItemRequest.java
@@ -1,21 +1,14 @@
 package org.vovgoo.orderservice.dto.orderItem.request;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import java.math.BigDecimal;
+import java.util.UUID;
 
 public record AddOrderItemRequest(
-        @NotNull(message = "ID блюда обязателен")
-        Long dishId,
+        @NotNull(message = "Идентификатор блюда обязателен")
+        UUID dishId,
 
         @NotNull(message = "Количество обязано быть заполнено")
         @Min(value = 1, message = "Количество должно быть не меньше 1")
-        Long quantity,
-
-        @NotNull(message = "Цена обязательна")
-        @DecimalMin(value = "0.01", message = "Цена должна быть положительной")
-        @Digits(integer = 8, fraction = 2, message = "Цена должна быть числом с максимум 2 знаками после запятой")
-        BigDecimal price
+        Long quantity
 ) {}
