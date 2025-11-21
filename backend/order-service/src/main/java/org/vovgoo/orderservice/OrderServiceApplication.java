4773c18084798b2e83036442857430a62abab363
Vova <vov.gonchar2017@yandex.ru>
2025-11-21 12:12:18 +0300

    configurate main file add scan packages from common

--- backend/order-service/src/main/java/org/vovgoo/orderservice/OrderServiceApplication.java
+++ backend/order-service/src/main/java/org/vovgoo/orderservice/OrderServiceApplication.java
@@ -3,7 +3,7 @@ package org.vovgoo.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.vovgoo.orderservice", "org.vovgoo.config", "org.vovgoo.security", "org.vovgoo.user"})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
