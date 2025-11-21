1ff441ed67c83563ff881dcc9bc73c17296813b8
Vova <vov.gonchar2017@yandex.ru>
2025-11-21 19:09:59 +0300

    refactor order item mapper, add method for list map

--- backend/order-service/src/main/java/org/vovgoo/orderservice/mapper/OrderItemMapper.java
+++ backend/order-service/src/main/java/org/vovgoo/orderservice/mapper/OrderItemMapper.java
@@ -6,6 +6,9 @@ import org.vovgoo.dto.dish.DishShortResponse;
 import org.vovgoo.orderservice.dto.orderItem.response.OrderItemResponse;
 import org.vovgoo.orderservice.entity.OrderItem;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring", uses = DishMapper.class)
 public interface OrderItemMapper {
 
@@ -14,4 +17,18 @@ public interface OrderItemMapper {
     @Mapping(source = "orderItem.quantity", target = "quantity")
     @Mapping(source = "orderItem.price", target = "price")
     OrderItemResponse toResponse(OrderItem orderItem, DishShortResponse dish);
+
+    default List<OrderItemResponse> toResponseList(List<OrderItem> orderItems, List<DishShortResponse> dishes) {
+        return orderItems.stream()
+                .map(oi -> {
+                    DishShortResponse dish = dishes.stream()
+                            .filter(d -> d.id().equals(oi.getDishId()))
+                            .findFirst()
+                            .orElseThrow(() -> new RuntimeException(
+                                    "Dish not found for OrderItem " + oi.getId()
+                            ));
+                    return toResponse(oi, dish);
+                })
+                .collect(Collectors.toList());
+    }
 }
