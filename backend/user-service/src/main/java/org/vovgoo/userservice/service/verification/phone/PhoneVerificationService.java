b700a6a128691292accad16017c01cd7594ea49f
Vova <vov.gonchar2017@yandex.ru>
2025-11-23 23:18:07 +0300

    refacotr verification leyaer

--- backend/user-service/src/main/java/org/vovgoo/userservice/service/verification/phone/PhoneVerificationService.java
+++
@@ -1,8 +0,0 @@
-package org.vovgoo.userservice.service.verification.phone;
-
-import org.vovgoo.userservice.service.verification.phone.enums.PhoneVerificationType;
-
-public interface PhoneVerificationService {
-    void send(String phone, PhoneVerificationType type);
-    void validate(String phone, String code, PhoneVerificationType type);
-}
