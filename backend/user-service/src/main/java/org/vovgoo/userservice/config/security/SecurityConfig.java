625a7be67241fe7397ea8e709ec51bfacfc9ef2e
Vova <vov.gonchar2017@yandex.ru>
2025-11-23 23:19:41 +0300

    refactor security config

--- backend/user-service/src/main/java/org/vovgoo/userservice/config/security/SecurityConfig.java
+++ backend/user-service/src/main/java/org/vovgoo/userservice/config/security/SecurityConfig.java
@@ -1,39 +1,39 @@
-package org.vovgoo.userservice.config.security;
+    package org.vovgoo.userservice.config.security;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.vovgoo.security.filters.HeaderAuthenticationFilter;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+    import org.springframework.security.config.http.SessionCreationPolicy;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+    import org.vovgoo.security.filters.HeaderAuthenticationFilter;
 
-@Configuration
-@EnableMethodSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
+    @Configuration
+    @EnableMethodSecurity
+    @RequiredArgsConstructor
+    public class SecurityConfig {
 
-    public static final String DUMMY_PASSWORD_HASH = "$2a$10$e0MYzXyjpJS7Pd0RVvHwHeFX6A1/1omdJzBz4u2lghhVQb1z1u8Vu";
+        public static final String DUMMY_PASSWORD_HASH = "$2a$10$e0MYzXyjpJS7Pd0RVvHwHeFX6A1/1omdJzBz4u2lghhVQb1z1u8Vu";
 
-    private final HeaderAuthenticationFilter headerAuthenticationFilter;
+        private final HeaderAuthenticationFilter headerAuthenticationFilter;
 
-    @Bean
-    public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        @Bean
+        public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
+            return http.build();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
