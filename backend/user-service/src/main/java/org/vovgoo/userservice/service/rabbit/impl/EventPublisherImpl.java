ce52dbd9ec73c2cc1165e42224d148bea8db158f
Vova <vov.gonchar2017@yandex.ru>
2025-11-24 11:44:28 +0300

    add @Retryable on EventPublisher

--- backend/user-service/src/main/java/org/vovgoo/userservice/service/rabbit/impl/EventPublisherImpl.java
+++ backend/user-service/src/main/java/org/vovgoo/userservice/service/rabbit/impl/EventPublisherImpl.java
@@ -3,10 +3,14 @@ package org.vovgoo.userservice.service.rabbit.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.vovgoo.userservice.config.rabbit.RabbitMQConfig;
 import org.vovgoo.userservice.domain.rabbit.key.EventKey;
+import org.vovgoo.userservice.exception.custom.user.UserNotFoundException;
 import org.vovgoo.userservice.service.rabbit.EventPublisher;
 
 @Service
@@ -18,6 +22,10 @@ public class EventPublisherImpl implements EventPublisher {
 
     private static final String EXCHANGE = RabbitMQConfig.EXCHANGE;
 
+    @Retryable(
+            retryFor = AmqpException.class,
+            backoff = @Backoff(delay = 2000)
+    )
     public <T> void publish(EventKey<T> eventKey, T payload) {
         if (!eventKey.payloadType().isAssignableFrom(payload.getClass())) {
             throw new IllegalArgumentException(
