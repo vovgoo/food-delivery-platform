a4983b9e574247965870482fc72f8f7654976d85
Vova <vov.gonchar2017@yandex.ru>
2025-11-24 12:16:32 +0300

    add schema on ConfigrmChangePhoneRequest

--- backend/user-service/src/main/java/org/vovgoo/userservice/dto/user/request/ConfirmChangePhoneRequest.java
+++ backend/user-service/src/main/java/org/vovgoo/userservice/dto/user/request/ConfirmChangePhoneRequest.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+@Schema(description = "Request to confirm a user's phone change using a verification code")
 public record ConfirmChangePhoneRequest(
 
         @NotBlank(message = "Код не может быть пустым")
