a70b28f9212c379c71d49d137eb19e441e6db579
Vova <vov.gonchar2017@yandex.ru>
2025-11-25 16:18:09 +0300

    fix pro,blem with ocnfiguration and property files

--- backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/config/image/ImgbbProperties.java
+++ backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/config/image/ImgbbProperties.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "app.service.imgbb")
+@ConfigurationProperties(prefix = "app.image.service.imgbb")
 public class ImgbbProperties {
     private String apiKey;
     private String endpoint;
