54fdb2e330334edbfe580d3f408d774d5c1e69af
Vova <vov.gonchar2017@yandex.ru>
2025-12-03 12:32:58 +0300

    create restaurant admin card and breadcrumb

---
+++ frontend/src/components/restaurant/AdminRestaurantBreadcrumb.tsx
@@ -0,0 +1,42 @@
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Link } from "react-router-dom";
+import { Skeleton } from "../ui/skeleton";
+import { AppRoutes } from "@/routes";
+import { type RestaurantResponse } from "@/api";
+
+interface AdminRestaurantBreadcrumbProps {
+  restaurant?: RestaurantResponse;
+  isPending: boolean;
+}
+
+export const AdminRestaurantBreadcrumb: React.FC<AdminRestaurantBreadcrumbProps> = ({
+  restaurant,
+  isPending,
+}) => (
+  <Breadcrumb>
+    <BreadcrumbList>
+      <BreadcrumbItem>
+        <BreadcrumbLink asChild>
+          <Link to={AppRoutes.ADMIN_RESTAURANTS}>Рестораны</Link>
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbPage>
+          {isPending ? (
+            <Skeleton className="h-5 w-24 bg-gray-400 rounded" />
+          ) : (
+            restaurant?.name ?? "Без названия"
+          )}
+        </BreadcrumbPage>
+      </BreadcrumbItem>
+    </BreadcrumbList>
+  </Breadcrumb>
+);
