80ace7f76fe1bb6f8186b1eed70b55458b626b50
Vova <vov.gonchar2017@yandex.ru>
2025-12-03 12:34:52 +0300

    create restaurants admin page

---
+++ frontend/src/pages/admin/AdminRestaurantsPage.tsx
@@ -0,0 +1,17 @@
+import { AdminRestaurantList } from "@/features/restaurant/AdminRestaurantList";
+import { CreateRestaurantFormDialog } from "@/features/restaurant/CreateRestaurantFormDialog";
+import React from "react";
+
+const AdminRestaurantsPage: React.FC = () => {
+  return (
+    <div className="w-full">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold">Список ресторанов</h1>
+        <CreateRestaurantFormDialog />
+      </div>
+      <AdminRestaurantList pageSize={6} />
+    </div>
+  );
+};
+
+export default AdminRestaurantsPage;
