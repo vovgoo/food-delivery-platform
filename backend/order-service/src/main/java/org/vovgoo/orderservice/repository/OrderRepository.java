03c782ece917e7e6aafa7a4786add49e48a8d080
Vova <vov.gonchar2017@yandex.ru>
2025-12-04 12:26:55 +0300

    fix probmlem with join query when user get orders

--- backend/order-service/src/main/java/org/vovgoo/orderservice/repository/OrderRepository.java
+++ backend/order-service/src/main/java/org/vovgoo/orderservice/repository/OrderRepository.java
@@ -14,6 +14,6 @@ public interface OrderRepository extends JpaRepository<Order,UUID> {
     @Query("SELECT o FROM Order o LEFT JOIN FETCH o.items LEFT JOIN FETCH o.payment")
     Page<Order> findAllWithItemsAndPayment(Pageable pageable);
 
-    @Query("SELECT o FROM Order o LEFT JOIN FETCH o.items LEFT JOIN FETCH o.payment WHERE o.userId = :userId")
+    @Query("SELECT o FROM Order o WHERE o.userId = :userId")
     Page<Order> findByUserIdWithItemsAndPayment(@Param("userId") UUID userId, Pageable pageable);
 }
