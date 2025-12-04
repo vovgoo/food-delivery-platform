8cc359dc9d8ffe6274691680ec9ac506e7c6b417
Vova <vov.gonchar2017@yandex.ru>
2025-12-04 14:26:17 +0300

    update repository. add sorting for pageable query

--- backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/repository/RestaurantRepository.java
+++ backend/restaurant-service/src/main/java/org/vovgoo/restaurantservice/repository/RestaurantRepository.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
 
-    @Query("SELECT r FROM Restaurant r WHERE r.status <> 'CLOSED' AND (:cuisine IS NULL OR LOWER(r.cuisine) LIKE LOWER(CONCAT('%', :cuisine, '%')))")
+    @Query("SELECT r FROM Restaurant r WHERE r.status <> 'CLOSED' AND (:cuisine IS NULL OR LOWER(r.cuisine) LIKE LOWER(CONCAT('%', :cuisine, '%'))) ORDER BY r.name ASC")
     Page<Restaurant> findByCuisineContainingIgnoreCase(String cuisine, Pageable pageable);
 
     @Query("SELECT r FROM Restaurant r WHERE r.id = :id AND r.status <> 'CLOSED'")
--- backend/user-service/src/main/java/org/vovgoo/userservice/repository/AddressRepository.java
+++ backend/user-service/src/main/java/org/vovgoo/userservice/repository/AddressRepository.java
@@ -20,7 +20,7 @@ public interface AddressRepository extends JpaRepository<Address, UUID> {
     @Query("SELECT a FROM Address a WHERE a.user.id = :userId AND a.isDefault = true AND a.addressStatus = 'ACTIVE'")
     Optional<Address> findDefaultByUserId(@Param("userId") UUID userId);
 
-    @Query("SELECT a FROM Address a WHERE a.user.id = :userId AND a.addressStatus = 'ACTIVE'")
+    @Query("SELECT a FROM Address a WHERE a.user.id = :userId AND a.addressStatus = 'ACTIVE' ORDER BY a.country")
     Page<Address> findAllByUserIdAndAddressStatus(Pageable pageable, @Param("userId") UUID userId);
 
     @Query("SELECT a FROM Address a WHERE a.id = :id AND a.user.id = :userId")
